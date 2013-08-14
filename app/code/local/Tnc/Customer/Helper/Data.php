b17302f80bd0ff745b73268382d207c8e4bb977a
revlis7 <silver47@yeah.net>
2013-08-14 15:53:42 +0800

    clean code

--- app/code/local/Tnc/Customer/Helper/Data.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Tnc_Customer_Helper_Data extends Mage_Customer_Helper_Data
-{
-    public function getLoginUrl()
-    {
-        return $this->_getUrl(self::ROUTE_ACCOUNT_LOGIN, $this->getLoginUrlParams());
-    }
-}
---
+++ app/code/local/Tnc/Member/controllers/LoginController.php
@@ -0,0 +1,16 @@
+<?php
+
+class Tnc_Member_LoginController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+    public function goodbyeAction()
+    {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+}
